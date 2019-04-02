d20094df693d5a714495aa56359da87cda309b3a
Adam Corpora <adam.corpora@gmail.com>
2019-04-01 20:40:11 -0400

    Added the reverse order and clarified variable names.

--- src/com/company/Input.java
+++ src/com/company/Input.java
@@ -6,26 +6,30 @@ import java.util.*;
 public class Input {
     public static void main(String[] args) {
 
-        ArrayList<String> words = new ArrayList<>();//creates an Array named words
+        ArrayList<String> arrayWords = new ArrayList<>();//creates an Array named words
         StringBuilder builder = new StringBuilder();
 
-        System.out.print("Enter a string: ");
-        Scanner adam = new Scanner(System.in);      //whatever you type in, store it in scanner "adam"
-        String verb = adam.next();                 //convert "adam" to a string "verb"
-        while (!verb.equals("exit")) {
-            System.out.print("Enter a string: ");
-            words.add(verb);                //add another string "verb" to the array "words"
-            verb = adam.next();             //string "verb" now equals whatever is next in scanner "adam"
+        System.out.print("Enter a string. (Type exit to quit): ");
+        Scanner scannerAdam = new Scanner(System.in);           //whatever you type in, store it in scanner "adam"
+        String stringVerb = scannerAdam.next();                 //convert "adam" to a string "verb". asks for input
+        while (!stringVerb.equals("exit")) {
+            System.out.print("Enter a string. (Type exit to quit): ");
+            arrayWords.add(stringVerb);                //add another string "verb" to the array "words"
+            stringVerb = scannerAdam.next();             //string "verb" now equals whatever is next in scanner "adam"
         }
 
-        if (verb.equals("exit")) {
-            System.out.println("Thank you");
-            for (String sText : words) {
+        if (stringVerb.equals("exit")) {
+            System.out.println("Thank you. Your strings are:");
+            System.out.println(arrayWords);
+            System.out.println("\n");
+            System.out.println("Each string backward is:");
+            for (String sText : arrayWords) {
                 String sReverse = new StringBuilder(sText).reverse().toString();
                 System.out.println(sReverse);
-
-
+            }
+            System.out.println("\n" + "Your strings in reverse order are:");
+            Collections.reverse(arrayWords);
+            System.out.println(arrayWords);
+            }
             }
         }
-    }
-}
